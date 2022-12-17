a5e5fafc20292fda9dd43511e5b2fa9bbc8c8ba7
mrsekut <k.cloudspider@gmail.com>
2022-12-17 19:15:12 +0900

    feat: update some libraries

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,54 +3,52 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 const Header: React.FC = () => {
-  const router = useRouter();
-  const isActive: (pathname: string) => boolean = (pathname) =>
-    router.pathname === pathname;
+	const router = useRouter();
+	const isActive: (pathname: string) => boolean = (pathname) =>
+		router.pathname === pathname;
 
-  let left = (
-    <div className="left">
-      <Link href="/">
-        <a className="bold" data-active={isActive("/")}>
-          Feed
-        </a>
-      </Link>
-      <style jsx>{`
-        .bold {
-          font-weight: bold;
-        }
+	let left = (
+		<div className="left">
+			<Link href="/" className="bold" data-active={isActive("/")}>
+				Feed
+			</Link>
+			<style jsx>{`
+				.bold {
+					font-weight: bold;
+				}
 
-        a {
-          text-decoration: none;
-          color: #000;
-          display: inline-block;
-        }
+				a {
+					text-decoration: none;
+					color: #000;
+					display: inline-block;
+				}
 
-        .left a[data-active="true"] {
-          color: gray;
-        }
+				.left a[data-active="true"] {
+					color: gray;
+				}
 
-        a + a {
-          margin-left: 1rem;
-        }
-      `}</style>
-    </div>
-  );
+				a + a {
+					margin-left: 1rem;
+				}
+			`}</style>
+		</div>
+	);
 
-  let right = null;
+	let right = null;
 
-  return (
-    <nav>
-      {left}
-      {right}
-      <style jsx>{`
-        nav {
-          display: flex;
-          padding: 2rem;
-          align-items: center;
-        }
-      `}</style>
-    </nav>
-  );
+	return (
+		<nav>
+			{left}
+			{right}
+			<style jsx>{`
+				nav {
+					display: flex;
+					padding: 2rem;
+					align-items: center;
+				}
+			`}</style>
+		</nav>
+	);
 };
 
 export default Header;
